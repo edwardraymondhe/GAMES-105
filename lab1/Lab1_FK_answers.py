7ddcca8cc8b2907ef17920974772a0dc2eaef4a3
edwardraymondhe <43778680+edwardraymondhe@users.noreply.github.com>
2024-09-16 23:18:11 +0800

    feat: lab1_task3, i am finally happy today :)

--- lab1/Lab1_FK_answers.py
+++ lab1/Lab1_FK_answers.py
@@ -12,6 +12,8 @@ class Joint:
         self.children = []
         self.parent = None
         
+        self.retarget = R.identity()
+        
     def log(self):
         print(self.name)
         for i in range(len(self.children)):
@@ -27,6 +29,8 @@ class Joint:
             # Pi = P_pi + Q_i * l
             self.orientation = self.parent.orientation * self.rotation
             self.position = self.parent.position + self.parent.orientation.apply(self.translation)
+            # print(f"{self.name}\n{self.rotation.as_matrix()}")
+            # print(f"{self.orientation.as_matrix()}")
         else:        
             # Root
             self.orientation = self.rotation
@@ -35,23 +39,60 @@ class Joint:
         for i in range(len(self.children)):
             self.children[i].fk()
             
-    def create_list(joint_name, joint_parent, joint_offset):
-        joint_list = []
-        for i in range(0, len(joint_parent)):
-            pi = joint_parent[i]
+def create_list(joint_name, joint_parent, joint_offset):
+    joint_list = []
+    for i in range(0, len(joint_parent)):
+        pi = joint_parent[i]
 
-            child = Joint(i, joint_name[i], joint_offset[i])
-            joint_list.append(child)
+        child = Joint(i, joint_name[i], joint_offset[i])
+        joint_list.append(child)
+        
+        if i != 0:
+            parent = joint_list[pi]
+            parent.children.append(child)
+        else:
+            parent = None
             
-            if i != 0:
-                parent = joint_list[pi]
-                parent.children.append(child)
-            else:
-                parent = None
-                
-            child.parent = parent
+        child.parent = parent
+        
+    return joint_list
+
+def get_joint_by_name(joint_list, joint_name):
+    for i in range(len(joint_list)):
+        if joint_list[i].name == joint_name:
+            return joint_list[i]
             
-        return joint_list
+def retarget_to_pose(joint_origin, joint_target):
+    print(f"Retargeting {joint_origin.name} \n{joint_origin.orientation.as_matrix()}\n{joint_target.orientation.as_matrix()}")
+
+    # Retarget to pose
+    if joint_origin.parent != None:
+        q_retarget = joint_target.orientation.inv() * joint_origin.parent.retarget * joint_origin.orientation
+    else:
+        q_retarget = joint_target.orientation.inv() * joint_origin.orientation
+        
+    joint_origin.retarget = q_retarget
+    
+    joint_origin_children = joint_origin.children
+    for i in range(len(joint_origin_children)):
+        retarget_to_pose(joint_origin_children[i], get_joint_by_name(joint_origin_children, joint_origin_children[i].name))
+        
+def parse_motion_data(joint_list, motion_data_curr, frame_id):
+    # Parse Root translation
+    joint_list[0].translation = motion_data_curr[0:3]
+
+    # Parse Joint rotations
+    read_idx = 0
+    for curr_idx in range(0, len(joint_list)):
+        curr = joint_list[curr_idx]
+        r_r = R.identity()
+        
+        if len(curr.children) != 0:
+            r_euler = motion_data_curr[3 + (read_idx * 3) : 3 + (read_idx * 3 + 3)]
+            r_r = R.from_euler('XYZ', r_euler, degrees=True)
+            read_idx += 1
+        
+        curr.rotation = r_r
 
 def load_motion_data(bvh_file_path):
     """part2 辅助函数，读取bvh文件"""
@@ -117,6 +158,41 @@ def part1_calculate_T_pose(bvh_file_path):
     return joint_name, joint_parent, joint_offset
 
 
+def get_unit_vector(vec):
+    magnitude = np.linalg.norm(vec)
+    if magnitude == 0:
+        return vec
+    else:
+        return vec / magnitude
+
+def get_angle_between_unit_vectors(a, b):
+    dot_product = np.dot(a, b)
+    return np.arccos(dot_product)
+
+def get_rotation_matrix(a, b):
+    a = get_unit_vector(a)
+    b = get_unit_vector(b)
+    u = get_unit_vector(np.cross(a, b))
+    
+    theta = get_angle_between_unit_vectors(a, b)
+    sin_theta = np.sin(theta)
+    cos_theta = np.cos(theta)
+    matrix = np.array(
+            [[0, -u[2], u[1]],
+            [u[2], 0, -u[0]],
+            [-u[1], u[0], 0]]
+        )
+    
+    r = R.identity().as_matrix() + sin_theta * matrix + (1 - cos_theta) * matrix * matrix
+    
+    return r
+
+def list_equal_within_tolerance(list1, list2):
+    array1 = np.array(list1)
+    array2 = np.array(list2)
+    # 使用allclose方法判断两数组是否在指定精度内相等
+    return np.allclose(array1, array2, atol=0.001)
+
 def part2_forward_kinematics(joint_name, joint_parent, joint_offset, motion_data, frame_id):
     """请填写以下内容
     输入: part1 获得的关节名字，父节点列表，偏移量列表
@@ -131,34 +207,19 @@ def part2_forward_kinematics(joint_name, joint_parent, joint_offset, motion_data
     """
     
     # Create joint list
-    joint_list = Joint.create_list(joint_name, joint_parent, joint_offset)
+    joint_list = create_list(joint_name, joint_parent, joint_offset)
     
     # Get motion data for current frame
     motion_data_curr = motion_data[frame_id]
-    joint_len = len(joint_name)
     joint_positions = []
     joint_orientations = []
     
-    # Parse Root translation
-    joint_list[0].translation = motion_data_curr[0:3]
-
-    # Parse Joint rotations
-    read_idx = 0
-    for curr_idx in range(0, joint_len):
-        curr = joint_list[curr_idx]
-        r_r = R.identity()
-        
-        if len(curr.children) != 0:
-            r_euler = motion_data_curr[3 + (read_idx * 3) : 3 + (read_idx * 3 + 3)]
-            r_r = R.from_euler('XYZ', r_euler, degrees=True)
-            read_idx += 1
-        
-        curr.rotation = r_r
+    parse_motion_data(joint_list, motion_data_curr, frame_id)
         
     # Recursive fk() from Root
     joint_list[0].fk()
        
-    for i in range(joint_len):
+    for i in range(len(joint_list)):
         joint_positions.append(joint_list[i].position)
         joint_orientations.append(joint_list[i].orientation.as_quat())
     return np.array(joint_positions), np.array(joint_orientations)
@@ -174,8 +235,60 @@ def part3_retarget_func(T_pose_bvh_path, A_pose_bvh_path):
         两个bvh的joint name顺序可能不一致哦(
         as_euler时也需要大写的XYZ
     """
+    joint_name_t, joint_parent_t, joint_offset_t = part1_calculate_T_pose(T_pose_bvh_path)
+    joint_name_a, joint_parent_a, joint_offset_a = part1_calculate_T_pose(A_pose_bvh_path)
     
+    joint_list_t = create_list(joint_name_t, joint_parent_t, joint_offset_t)
+    joint_list_a = create_list(joint_name_a, joint_parent_a, joint_offset_a)
+
+    joint_list_t[0].fk()
+    joint_list_a[0].fk()
     
+    # Calculate retarget matrix
+    for i in range(len(joint_list_t)):
+        joint_t = joint_list_t[i]
+        joint_a = get_joint_by_name(joint_list_a, joint_t.name)
+        joint_a.retarget = R.identity()
+        if joint_a.idx != 0:
+            if len(joint_a.children) == 1:
+                joint_t_trans_dir = joint_t.translation
+                joint_t_child_trans_dir = joint_t.children[0].translation
+                joint_t_relative_matrix = get_rotation_matrix(joint_t_trans_dir, joint_t_child_trans_dir)
+                
+                joint_a_trans_dir = joint_a.translation
+                joint_a_child_trans_dir = joint_a.children[0].translation
+                joint_a_relative_matrix = get_rotation_matrix(joint_a_trans_dir, joint_a_child_trans_dir)
+                
+                if (not (joint_t_relative_matrix == joint_a_relative_matrix).all()):
+                    retarget_rotation = R.from_matrix(joint_t_relative_matrix) * R.from_matrix(joint_a_relative_matrix).inv()
+                    joint_a.retarget = retarget_rotation
+        
+    # Load raw data
+    motion_data_a = load_motion_data(A_pose_bvh_path)
     
-    motion_data = None
-    return motion_data
+    # Calculate retargeting
+    # Output according to t's structure
+    motion_data_t = []
+    # Every frame
+    for i in range(len(motion_data_a)):
+        motion_data_t_curr = []
+        motion_data_a_curr = motion_data_a[i]
+        
+        # Parse and recalculate
+        parse_motion_data(joint_list_a, motion_data_a_curr, i)
+        joint_list_a[0].fk()
+        
+        motion_data_t_curr.extend(motion_data_a_curr[0:3])
+        
+        for j in range(len(joint_list_t)):
+            joint_t = joint_list_t[j]
+            if len(joint_t.children) == 0:
+                continue
+            
+            joint_a = get_joint_by_name(joint_list_a, joint_t.name)
+            rotation_a_t = (joint_a.rotation * joint_a.retarget.inv()).as_euler('XYZ', degrees=True)
+            motion_data_t_curr.extend(rotation_a_t)
+
+        motion_data_t.append(motion_data_t_curr)
+    
+    return np.array(motion_data_t)
